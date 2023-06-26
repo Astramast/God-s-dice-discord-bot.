1135f8bf769009d528937e8accc1fd2eca8cc5cb
Astramast <azerqsdfazerqsdg@gmail.com>
2023-06-26 19:26:19 +0200

    end of refactor for lance function

---
+++ CONFIG.py
@@ -0,0 +1,6 @@
+# Maximum amount of dices allowed in xdy (x value)
+DICE_AMOUNT_LIMIT = 1000
+# Maximum amount of messages allowed to be sent in one second.
+# Careful, this number MUST be under 50 => https://discord.com/developers/docs/topics/rate-limits
+MESSAGE_AMOUNT_LIMIT = 5
+MESSAGE_RATE = 1/MESSAGE_AMOUNT_LIMIT
--- main.py
+++ main.py
@@ -7,28 +7,42 @@ pré-alpha
 
 from random import randint
 from time import sleep
-import youtube_dl
+#import youtube_dl
 import asyncio
 import discord
 import discord.ext
 from discord.ext import commands
+from CONFIG import *
 
 """Définition de fonctions"""
 def spacer(_list):
+	spaced_str = ""
 	for element in _list:
-		print(element, end=' ')
-	print("")
+		spaced_str+=str(element)+" "
+	return spaced_str + '\n'
 
 def verif(xdy):
 	xdy_list = xdy.split("d")
 	if len(xdy_list) != 2:
 		return False
+
 	for element in xdy_list:
 		try:
 			int(element)
 		except ValueError:
 			return False
+
+	if int(xdy_list[0]) > DICE_AMOUNT_LIMIT:
+		return False
+
 	return True
+
+async def sendMessage(msg):
+	n = len(msg)//2000
+	for _ in range(n+1):
+		await ctx.send(msg[:2000])
+		msg = msg[2000:]
+		sleep(MESSAGE_RATE)
 
 # code
 
@@ -38,127 +52,86 @@ ytdl = youtube_dl.YoutubeDL()
 
 @bot.event
 async def on_ready():
-    print("Ready !")
+	print("Ready !")
 
 @bot.event
 async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.send("Mmmmmmh, j'ai bien l'impression que cette commande n'existe pas :/")
-    elif isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send("Il manque un argument.")
-    elif isinstance(error, commands.MissingPermissions):
-        await ctx.send("Vous n'avez pas les permissions pour faire cette commande.")
-    elif isinstance(error, commands.CheckFailure):
-        await ctx.send("Oups vous ne pouvez iutilisez cette commande.")
-    elif isinstance(error.original, discord.Forbidden):
-        await ctx.send("Oups, je n'ai pas les permissions nécéssaires pour faire cette commmande")
+	if isinstance(error, commands.CommandNotFound):
+		await ctx.send("Mmmmmmh, j'ai bien l'impression que cette commande n'existe pas :/")
+	elif isinstance(error, commands.MissingRequiredArgument):
+		await ctx.send("Il manque un argument.")
+	elif isinstance(error, commands.MissingPermissions):
+		await ctx.send("Vous n'avez pas les permissions pour faire cette commande.")
+	elif isinstance(error, commands.CheckFailure):
+		await ctx.send("Oups vous ne pouvez iutilisez cette commande.")
+	elif isinstance(error.original, discord.Forbidden):
+		await ctx.send("Oups, je n'ai pas les permissions nécéssaires pour faire cette commmande")
 
 
 @bot.command()
 async def coucou(ctx):
-    print("Coucou, tu veux voir ma bite ?")
-    await ctx.send("Coucou, tu veux voir ma bite ?")
+	print("Coucou, tu veux voir ma bite ?")
+	await ctx.send("Coucou, tu veux voir ma bite ?")
 
 
 @bot.command()
 async def lance(ctx, xdy, operation):
 	if not verif(xdy):
-        print("Erreur input")
-        await ctx.send(f"Tu as fait {erreur} erreurs. \n Un dé doit être décrit comme tel: \n \"xdy\" où x est le "
-                       f"nombre de dés et y le nombre de faces")
+		print("Erreur input")
+		await ctx.send(f"Tu as fait une erreur. \n Un dé doit être décrit comme tel: \n \t\"xdy\"\n où x est le \
+			nombre de dés et y le nombre de faces du dé. X est limité à 1000.")
 		return
-    print("lance")
-    info = xdy
-    if len(info) > 15:
-        await ctx.send("Arrète de faire n'importe quoi petit faquin")
-        info = "1d1"
-    b = 0
-    liste = ["", ""]
-    for j in range(len(info)):
-        if info[j] != "d":
-            liste[b] += info[j]
-        else:
-            b += 1
-    await ctx.send("Les dés sont lancé !!!")
-    res = []
-    c = liste[0]
-    c = int(c)
-    for i in range(c):
-        a = randint(1, int(liste[1]))
-        res.append(a)
-        a = 0
-    res_str = spacer(res)
-    if len(res_str) <= 1070 and len(operation) == 0:
-        message = f"Les résultats sont :{res_str}"
-        print(message)
-        await ctx.send(message)
-    if len(res_str) > 1070 and len(operation) == 0:
-        message = f"Les résultats sont :{res_str[: 1070]}"
-        await ctx.send(message)
-        l = 1
-        rang_str = len(res_str)
-        n_messages = rang_str // 2000
-        while l <= n_messages:
-            sleep(1)
-            n = l * 1070
-            l += 1
-            message = res_str[n: n + 1070]
-            await ctx.send(message)
-    else:
-        if operation == "somme":
-            somme = 0
-            for i in range(len(res_str) - 1):
-                somme += res_str[i]
-            message = f"Le résultat est :{somme}"
-            await ctx.send(message)
-        if operation[0] == "-":
-            i = 1
-            a = ""
-            while i <= len(res_str) - 1 :
-                a += operation[i]
-            for i in range(len(res_str) - 1):
-                res_str[i] -= int(a)
-                message = f"Les résultats sont :{res_str}"
-                print(message)
-                    await ctx.send(message)
+	print("lance")
+	xy = xdy.split("d")
+	await ctx.send("Les dés sont lancé !!!")
+	res = []
+	dice_amount = int(xy[0])
+	for i in range(dice_amount):
+		dice = randint(1, int(xy[1]))
+		res.append(dice)
 
-    else:
+	res_str = spacer(res)
+	message = f"Les résultats sont :{res_str}"
+	if operation == "+":
+		message = f"Le résultat est :{sum(res)}"
+	print(message)
+	sendMessage(message)
 
 
 @bot.command()
 async def spam(ctx, nombre, temps, *texte):
-    print("spam")
-    message = " ".join(texte)
-    for i in range(int(nombre) + 1):
-        await ctx.send(message)
-        sleep(int(temps))
+	print("spam")
+	message = " ".join(texte)
+	for i in range(int(nombre) + 1):
+		await ctx.send(message)
+		sleep(int(temps))
 
 
 @bot.command()
 async def compte(ctx, nombre, temps):
-    print("compte")
-    for i in range(int(nombre) + 1):
-        message = ""
-        message += str(i)
-        await ctx.send(message)
-        sleep(int(temps))
+	print("compte")
+	for i in range(int(nombre) + 1):
+		message = ""
+		message += str(i)
+		await ctx.send(message)
+		sleep(int(temps))
 
 
 @bot.command()
 async def kick(ctx, user: discord.User, *reason):
-    print(reason)
-    reason = " ".join(reason)
-    await ctx.guild.kick(user, reason=reason)
-    await ctx.send(
-        f"{user} à été détruit par le dés de dieux, si il revient cela donnera une preuve de plus qu'il est l'antéchriste ")
+	print(reason)
+	reason = " ".join(reason)
+	await ctx.guild.kick(user, reason=reason)
+	await ctx.send(
+		f"{user} à été détruit par le dés de dieux, si il revient cela donnera une preuve de plus qu'il est l'antéchriste ")
 
 
 
 @bot.command()
 async def ban(ctx, user: discord.User, *reason):
-    reason = " ".join(reason)
-    await ctx.guild.ban(user, reason=reason)
-    await ctx.send(f"{user} à été ban pour la raison suivante : {reason}.")
+	reason = " ".join(reason)
+	await ctx.guild.ban(user, reason=reason)
+	await ctx.send(f"{user} à été ban pour la raison suivante : {reason}.")
 
 
 
@@ -166,15 +139,15 @@ async def ban(ctx, user: discord.User, *reason):
 """
 @bot.command()
 async def serverInfo(ctx):
-    print("servinfo")
-    server = ctx.guild
-    numberOfTextChannels = len(server.text_channels)
-    numberOfVoiceChannels = len(server.voice_channels)
-    serverDescription = server.description
-    numberOfPerson = server.member_count
-    serverName = server.name
-    message = f"Le serveur **{serverName}** contient *{numberOfPerson}* personnes ! \nLa description du serveur est {serverDescription}. \nCe serveur possède {numberOfTextChannels} salons écrit et {numberOfVoiceChannels} salon vocaux."
-    await ctx.send(message)
+	print("servinfo")
+	server = ctx.guild
+	numberOfTextChannels = len(server.text_channels)
+	numberOfVoiceChannels = len(server.voice_channels)
+	serverDescription = server.description
+	numberOfPerson = server.member_count
+	serverName = server.name
+	message = f"Le serveur **{serverName}** contient *{numberOfPerson}* personnes ! \nLa description du serveur est {serverDescription}. \nCe serveur possède {numberOfTextChannels} salons écrit et {numberOfVoiceChannels} salon vocaux."
+	await ctx.send(message)
 """
 
 bot.run("NzYxMTU1ODYyODM5NTU4MTY0.X3Wfow.eAsaacpeNbK2y_ajGU_OxcDK6_Y")
